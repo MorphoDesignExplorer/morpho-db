2e717fe9cc99193d471b334dd4a9c894d38f7996
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2024-03-29 12:06:36 -0400

    ironed out deployment configs

---
+++ gunicorn_config.py
@@ -0,0 +1,8 @@
+import multiprocessing
+
+accesslog = "-"  # stdin for journalctl
+workers = multiprocessing.cpu_count() * 2 + 1
+# bind = "127.0.0.1:8000" # Debug Config
+bind = "unix:/run/gunicorn.sock"
+raw_env = ["DJANGO_SETTINGS_MODULE=backend.production_settings"]
+wsgi_app = "backend.wsgi"
